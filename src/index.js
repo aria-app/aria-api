b716eb79d4ac38abefac87b6adc073e328530eee
Nick Johnson <nickjohnson.dev@gmail.com>
2020-11-23 10:43:46 -0600

    Get basic song model hooked up

---
+++ src/index.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const { ApolloServer, gql } = require('apollo-server');
+const mongoose = require('mongoose');
+
+const Song = require('./models/Song');
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const typeDefs = gql`
+  type Query {
+    songs: [Song]
+  }
+
+  type Song {
+    name: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    songs: () => Song.find({}),
+  },
+};
+
+const server = new ApolloServer({ resolvers, typeDefs });
+
+server.listen(process.env.PORT).then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
