65878e8692a205ccd205b0697f757d9e0de87298
Nick Johnson <nickjohnson.dev@gmail.com>
2021-03-17 14:33:55 -0500

    Initial schema definition

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 
+const { PrismaClient } = require('@prisma/client');
 const db = require('./db');
 const getServer = require('./getServer');
 
+const prisma = new PrismaClient();
+console.log(prisma);
 const server = getServer({ db });
 
 server.listen(process.env.PORT).then(({ url }) => {
